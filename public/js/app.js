75ab5ee2c5ef7a8080f2464cf5e6b6735c69a654
Nikhil Dhakal <nickarsenal007@gmail.com>
2017-07-29 18:27:42 +0545

    Styling the Navigation

--- public/js/app.js
+++ public/js/app.js
@@ -792,6 +792,17 @@ window.Vue = __webpack_require__(34);
 
 Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
 
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
+
+$(document).ready(function () {
+  $('button.dropdown').hover(function (e) {
+    $(this).toggleClass('is-open');
+  });
+});
+
 /***/ }),
 /* 10 */
 /***/ (function(module, exports, __webpack_require__) {
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,3 +5,14 @@ window.Vue = require('vue');
 import Buefy from "buefy";
 
 Vue.use(Buefy);
+
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
+
+$(document).ready(function(){
+  $('button.dropdown').hover(function(e){
+    $(this).toggleClass('is-open');
+  });
+});
